ce6e0844662f552567b444ef765eead48f019b38
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-09-24 09:51:59 -0400

    printArray

--- F25Week4IntroToGenerics/Program.cs
+++ F25Week4IntroToGenerics/Program.cs
@@ -8,6 +8,8 @@
 
             object o = i;   // boxing
 
+            object o2 = 6;
+
             i = (int)o;     // unboxing
 
             //if (AreEqual(5, "hello"))
@@ -15,6 +17,25 @@
                 Console.WriteLine("Both are equal");
             else
                 Console.WriteLine("Both are not equal");
+
+
+
+            int[] intArr = { 3, 4, 5, 6, 4 };
+            double[] doubleArr = { 3.3, 5.5, 7.7, 2.2 };
+            char[] charArr = { 'h', 'e', 'l', 'l', 'o' };
+
+            PrintArray<int>(intArr);
+            PrintArray<double>(doubleArr);
+            PrintArray<char>(charArr);
+        }
+
+        static void PrintArray<T>(T[] array)
+        {
+            Console.WriteLine("\nNumber of items: " + array.Length);
+
+            foreach (T i in array)
+                Console.Write(i + " ");
+            Console.WriteLine();
         }
 
         // non-generic method
