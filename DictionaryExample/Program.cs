d58723a4c2cd31a79a0d7d89f6989bcaa0f74b99
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-09-26 09:23:19 -0400

    dict emp

---
+++ DictionaryExample/Program.cs
@@ -0,0 +1,36 @@
+﻿using ListExercise;
+
+namespace DictionaryExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Employee class is in ListExercise project
+            // we added a reference to that project
+            // included that project at the top by using the "using" statement
+
+            Employee emp1 = new Employee(101, "John", 5000);
+            Employee emp2 = new Employee(102, "Anne", 7000);
+            Employee emp3 = new Employee(103, "Mark", 3000);
+
+            Dictionary<int, Employee> dictEmps = new Dictionary<int, Employee>();
+            dictEmps.Add(emp1.Id, emp1);
+            dictEmps.Add(emp2.Id, emp2);
+            dictEmps.Add(emp3.Id, emp3);
+
+            //dictEmps.Add(emp1.Id, emp1);
+
+            Console.Write("Enter key: ");
+            int key = Convert.ToInt32(Console.ReadLine());
+
+            if (dictEmps.ContainsKey(key))
+            {
+                Employee emp = dictEmps[key];
+                Console.WriteLine(emp);
+            }
+            else
+                Console.WriteLine("Invalid key, please try again");
+        }
+    }
+}
