9bcc33876f6ecf785dfd259fbc6d8ffef3d236a4
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-09-26 09:40:42 -0400

    stack ex

--- StackAndQueueExamples/Program.cs
+++ StackAndQueueExamples/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Stack<int> stack = new Stack<int>();
+            stack.Push(10);
+            stack.Push(20);
+            stack.Push(30);
+            stack.Push(40);
+
+            Console.WriteLine("Total items: " + stack.Count);
+            foreach (var i in stack)
+                Console.WriteLine(i);
+
+            Console.WriteLine("\n\n");
+
+            //int num = stack[1];
+
+            int num = stack.Pop();
+            Console.WriteLine(num);
+
+            Console.WriteLine("Total items: " + stack.Count);
+            foreach (var i in stack)
+                Console.WriteLine(i);
+
+            Console.WriteLine("\n\n");
+
+
+            num = stack.Peek();
+            Console.WriteLine(num);
+
+            Console.WriteLine("Total items: " + stack.Count);
+            foreach (var i in stack)
+                Console.WriteLine(i);
         }
     }
 }
